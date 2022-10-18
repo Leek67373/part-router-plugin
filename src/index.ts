a2f2893b472d938d01ab2e37be5c7da8ced3cc88
wangpengyu <wangpengyu@e-sscard.com>
2022-10-18 16:22:13 +0800

    chore: release v0.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-export const one = 1
-export const two = 2
+class partRouterPlugin {
+    constructor(public path:string = '@/router/routes$') {
+    }
+    apply(compiler: any) {
+      const alias = (compiler.options && compiler.options.resolve && compiler.options.resolve.alias) || {};
+      compiler.options.resolve.alias = {
+        [this.path]: `${process.env.INIT_CWD}/index.js`,
+        ...alias
+      };
+    }
+  }
+  
+export default partRouterPlugin;
+  
